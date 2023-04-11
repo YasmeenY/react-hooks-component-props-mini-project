91a83d4c5028fffe99cb07ce31b86700f95d537c
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-04-11 10:14:11 -0400

    trying to pass tests on canvas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import React from "react";
 
-function Header({name}){
-    return(
-        <h1>{name}</h1>
+function Header({ name }) {
+    return (
+        <header>
+            <h1>{name}</h1>
+        </header>
     );
 }
 
